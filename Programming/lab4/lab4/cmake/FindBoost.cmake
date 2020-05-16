9f2dfc41abc52179fe1c3a2488729e6d5ac9bdbc
Roman Astrakhantsev <rgastrakhantsev@gmail.com>
2020-05-16 22:20:16 +0300

    images documet

--- Programming/lab4/lab4/cmake/FindBoost.cmake
+++
@@ -1,7 +0,0 @@
-add_library(Boost::Boost UNKNOWN IMPORTED)
-
-target_include_directories(Boost::Boost INTERFACE "${Boost_INCLUDE_DIRS}")
-
-set_target_properties(Boost::Boost PROPERTIES
-    IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
-    IMPORTED_LOCATION {${Boost_LIBS}})
