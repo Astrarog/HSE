ebe994b1a6143ccb030782edd783416bb440c1af
Roman Astrakhantsev <rgastrakhantsev@gmail.com>
2020-06-04 12:46:09 +0300

    sorted White C++ course

---
+++ Coursera/C++/Course 1. White/week 3/Сортировка целых чисел по модулю/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  
+  // считываем вектор
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int& x : v) {
+    cin >> x;
+  }
+
+  // сортируем
+  sort(begin(v), end(v),
+       // функция, сравнивающая abs(l) и abs(r) — модули чисел l и r
+       [](int l, int r) {
+         return abs(l) < abs(r);
+       }
+  );
+
+  // выводим отсорированный вектор
+  for (int x : v) {
+    cout << x << " ";
+  }
+
+  return 0;
+}
