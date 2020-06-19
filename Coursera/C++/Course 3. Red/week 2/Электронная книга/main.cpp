8e9dcf0861d6e9feff7b41b438c17285f1ca15fe
Roman Astrakhantsev <rgastrakhantsev@gmail.com>
2020-06-19 23:25:38 +0300

    one more

---
+++ Coursera/C++/Course 3. Red/week 2/Электронная книга/main.cpp
@@ -0,0 +1,97 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+
+using namespace std;
+
+
+class ReadingManager {
+public:
+  ReadingManager()
+      : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
+        sorted_users_(),
+        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+
+  void Read(int user_id, int page_count) {
+    users_and_pages_[user_id] = page_count;
+  }
+
+  double Cheer(int user_id) const {
+    if (users_and_pages_.count(user_id) == 0) {
+      return 0;
+    }
+    const int user_count = GetUserCount();
+    if (user_count == 1) {
+      return 1;
+    }
+    const int page_count = users_and_pages_.at(user_id);
+    auto user_iter = users_and_pages_.find(user_id);
+    int dist = std::distance(users_and_pages_.begin(), prev(user_iter));
+    if (dist == user_count) {
+        return 0;
+    }
+    // По умолчанию деление целочисленное, поэтому
+    // нужно привести числитель к типу double.
+    // Простой способ сделать это — умножить его на 1.0.
+    return dist * 1.0 / (user_count - 1);
+  }
+
+private:
+  // Статическое поле не принадлежит какому-то конкретному
+  // объекту класса. По сути это глобальная переменная,
+  // в данном случае константная.
+  // Будь она публичной, к ней можно было бы обратиться снаружи
+  // следующим образом: ReadingManager::MAX_USER_COUNT.
+  static const int MAX_USER_COUNT_ = 100'000;
+
+  vector<int> user_page_counts_;
+  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
+  vector<int> user_positions_; // позиции в векторе sorted_users_
+  map<int, int> users_and_pages_;
+
+  int GetUserCount() const {
+    return users_and_pages_.size();
+  }
+  void AddUser(int user_id) {
+    users_and_pages_[user_id] = 0;
+  }
+  void SwapUsers(int lhs_position, int rhs_position) {
+    const int lhs_id = sorted_users_[lhs_position];
+    const int rhs_id = sorted_users_[rhs_position];
+    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
+    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
+  }
+};
+
+
+int main() {
+  // Для ускорения чтения данных отключается синхронизация
+  // cin и cout с stdio,
+  // а также выполняется отвязка cin от cout
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ReadingManager manager;
+
+  int query_count;
+  cin >> query_count;
+
+  for (int query_id = 0; query_id < query_count; ++query_id) {
+    string query_type;
+    cin >> query_type;
+    int user_id;
+    cin >> user_id;
+
+    if (query_type == "READ") {
+      int page_count;
+      cin >> page_count;
+      manager.Read(user_id, page_count);
+    } else if (query_type == "CHEER") {
+      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+    }
+  }
+
+  return 0;
+}
