e39bf23f79eef457b95bef8602b137b346b9da94
Roman Astrakhantsev <rgastrakhantsev@gmail.com>
2020-07-01 13:31:22 +0300

    done week 3

--- Coursera/C++/Course 3. Red/week 2/Электронная книга/main.cpp
+++ Coursera/C++/Course 3. Red/week 2/Электронная книга/main.cpp
@@ -10,12 +10,20 @@ using namespace std;
 class ReadingManager {
 public:
   ReadingManager()
-      : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-        sorted_users_(),
-        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+      : users_and_pages_(),
+        rating (1001) {}
 
   void Read(int user_id, int page_count) {
+    if (users_and_pages_.count(user_id) == 0) {
+      AddUser(user_id);
+    }
+    int last_value=users_and_pages_[user_id];
     users_and_pages_[user_id] = page_count;
+
+    for (int i=(last_value + 1); i<=page_count; ++i)
+    {
+        ++ (rating[i]);
+    }
   }
 
   double Cheer(int user_id) const {
@@ -27,42 +35,22 @@ public:
       return 1;
     }
     const int page_count = users_and_pages_.at(user_id);
-    auto user_iter = users_and_pages_.find(user_id);
-    int dist = std::distance(users_and_pages_.begin(), prev(user_iter));
-    if (dist == user_count) {
-        return 0;
-    }
-    // По умолчанию деление целочисленное, поэтому
-    // нужно привести числитель к типу double.
-    // Простой способ сделать это — умножить его на 1.0.
-    return dist * 1.0 / (user_count - 1);
+
+    return (user_count - rating[page_count]) * 1.0 / (user_count - 1);
   }
 
 private:
-  // Статическое поле не принадлежит какому-то конкретному
-  // объекту класса. По сути это глобальная переменная,
-  // в данном случае константная.
-  // Будь она публичной, к ней можно было бы обратиться снаружи
-  // следующим образом: ReadingManager::MAX_USER_COUNT.
-  static const int MAX_USER_COUNT_ = 100'000;
+  map<int, int> users_and_pages_; // позиции в векторе sorted_users_
 
-  vector<int> user_page_counts_;
-  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-  vector<int> user_positions_; // позиции в векторе sorted_users_
-  map<int, int> users_and_pages_;
+  vector<int> rating;
 
   int GetUserCount() const {
-    return users_and_pages_.size();
-  }
+      return users_and_pages_.size();
+    }
   void AddUser(int user_id) {
     users_and_pages_[user_id] = 0;
   }
-  void SwapUsers(int lhs_position, int rhs_position) {
-    const int lhs_id = sorted_users_[lhs_position];
-    const int rhs_id = sorted_users_[rhs_position];
-    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-  }
+
 };
 
 
