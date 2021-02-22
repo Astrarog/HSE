31d04af93e1bcb1a4f84897e060016de9edb68a7
Roman Astrakhantsev <rgastrakhantsev@gmail.com>
2021-02-22 20:20:37 +0300

    Fix: proper name for functions and proper usage of constrains

--- СУБД/Индивидуальное задание/Constrains.sql
+++ СУБД/Индивидуальное задание/Constrains.sql
@@ -5,62 +5,61 @@ AS
 BEGIN
     IF (@data IS NULL OR @data > '')          
         RETURN CAST(1 AS bit)
-    RETURN CAST(0 AS bit)
+	RETURN CAST(0 AS bit)
 END
+
 GO
+
 USE [FilmLibrary]
 -- Ограничения на данные --
 
 ALTER TABLE [Актёр] ADD 
 	CONSTRAINT [Актёр_уже_родился] CHECK ([Дата_рождения] < GETDATE()),
 	CONSTRAINT [Актёр_родился_после_появления_фильмов] CHECK (YEAR([Дата_рождения]) > 1800),
-	CONSTRAINT [Если_есть_ФИО_то_оно_не_пусто] CHECK (nullOrNonEmpty([ФИО]) = 1),
-	CONSTRAINT [Если_есть_Город_то_он_не_пуст] CHECK (nullOrNonEmpty([Город_проживания]) = 1)
+	CONSTRAINT [Если_есть_ФИО_то_оно_не_пусто] CHECK (dbo.nullOrNonEmpty([ФИО]) = 1),
+	CONSTRAINT [Если_есть_Город_то_он_не_пуст] CHECK (dbo.nullOrNonEmpty([Город_проживания]) = 1)
 
 ALTER TABLE [Фильм] ADD 
 	CONSTRAINT [Фильм_вышел_или_скоро_выйдет] CHECK ([Год_выхода] < YEAR(GETDATE())+20),
 	CONSTRAINT [Фильм_вышел_появления_фильмов] CHECK (YEAR([Год_выхода]) > 1800),
-	CONSTRAINT [Если_есть_Название_то_оно_не_пусто] CHECK (nullOrNonEmpty([Название]) = 1),
-	CONSTRAINT [Если_есть_Жанр_то_он_не_пуст] CHECK (nullOrNonEmpty([Жанр]) = 1),
+	CONSTRAINT [Если_есть_Название_то_оно_не_пусто] CHECK (dbo.nullOrNonEmpty([Название]) = 1),
+	CONSTRAINT [Если_есть_Жанр_то_он_не_пуст] CHECK (dbo.nullOrNonEmpty([Жанр]) = 1),
 	CONSTRAINT [Если_есть_Длительность_то_она_не_пуста] CHECK ([Длительность] IS NULL OR [Длительность] > 0)
 
 
 ALTER TABLE [Киностудия] ADD 
 	CONSTRAINT [Киностудия_основана_или_скоро_будет] CHECK ([Дата_основания] < YEAR(GETDATE())+20),
 	CONSTRAINT [Киностудия_появилась_после_появления_фильмов] CHECK (YEAR([Дата_основания]) > 1800),
-	CONSTRAINT [Если_есть_Название_то_оно_не_пусто] CHECK (nullOrNonEmpty([Название]) = 1),
-	CONSTRAINT [Если_есть_Город_расположения_то_он_не_пуст] CHECK (nullOrNonEmpty([Город_расположения]) = 1)
+	CONSTRAINT [Если_есть_Название_Киностудии_то_оно_не_пусто] CHECK (dbo.nullOrNonEmpty([Название]) = 1),
+	CONSTRAINT [Если_есть_Город_расположения_то_он_не_пуст] CHECK (dbo.nullOrNonEmpty([Город_расположения]) = 1)
 
 GO
 
--- При удалении контракта выставляем данные в NULL, чтобы  сохранить информацию о контракте --
--- Когда обе стороны контракта в NULL, то срабатывает триггер на удаление контратка --
+-- При удалении контракта и связей Many-Many выставляем данные в NULL, чтобы  сохранить информацию о контракте/связи --
+-- Когда обе стороны контракта в NULL (или оба поля таблицы M-M), то срабатывает триггер на удаление сущности --
 
 ALTER TABLE [Контракты] ADD
-	CONSTRAINT [Контракты_при_удалении_Актёра] FOREIGN KEY ([Код_киностудии]) REFERENCES [Киностудия]([Код_киностудии])
+	CONSTRAINT [Контракты_при_удалении_Киностудии] FOREIGN KEY ([Код_киностудии]) REFERENCES [Киностудия]([Код_киностудии])
 		ON DELETE SET NULL
 		ON UPDATE SET NULL ,
-	CONSTRAINT [Контракты_при_удалении_Киностудии] FOREIGN KEY ([Код_актёра]) REFERENCES [Актёр]([Код_актёра])
+	CONSTRAINT [Контракты_при_удалении_Актёра] FOREIGN KEY ([Код_актёра]) REFERENCES [Актёр]([Код_актёра])
 		ON DELETE SET NULL
 		ON UPDATE SET NULL
 
--- TO DO: TRIGGER --
-GO
--- Ограничения на удаление/измнение зависимых сущностей при удалении родителей --
-
 ALTER TABLE [Актёр_Фильм] ADD
 	CONSTRAINT [А_при_АФ_удалении] FOREIGN KEY ([Код_актёра]) REFERENCES [Актёр]([Код_актёра])
-		ON DELETE CASCADE
-		ON UPDATE CASCADE ,
+		ON DELETE SET NULL
+		ON UPDATE SET NULL ,
 	CONSTRAINT [Ф_при_АФ_удалении] FOREIGN KEY ([Код_фильма]) REFERENCES [Фильм]([Код_фильма])
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+		ON DELETE SET NULL
+		ON UPDATE SET NULL
 
 ALTER TABLE [Киностудия_Фильм] ADD
 	CONSTRAINT [К_при_КФ_удалении] FOREIGN KEY ([Код_киностудии]) REFERENCES [Киностудия]([Код_киностудии])
-		ON DELETE CASCADE
-		ON UPDATE CASCADE ,
+		ON DELETE SET NULL
+		ON UPDATE SET NULL ,
 	CONSTRAINT [Ф_при_КФ_удалении] FOREIGN KEY ([Код_фильма]) REFERENCES [Фильм]([Код_фильма])
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+		ON DELETE SET NULL
+		ON UPDATE SET NULL
+-- TO DO: TRIGGERS --
 GO
--- СУБД/Индивидуальное задание/DB creation.sql
+++ СУБД/Индивидуальное задание/DB creation.sql
@@ -3,18 +3,18 @@ USE [FilmLibrary]
 CREATE TABLE [Актёр]
 ( 
 	[Код_актёра] int  IDENTITY(1,1) ,
-	[ФИО]             varchar(20)  NOT NULL ,
+	[ФИО]             varchar(100)  NOT NULL ,
 	[Дата_рождения] datetime  NULL ,
-	[Город_проживания] varchar(20)  NULL ,
+	[Город_проживания] varchar(50)  NULL ,
 	CONSTRAINT [XPKАктёр] PRIMARY KEY  NONCLUSTERED ([Код_актёра] ASC)
 )
 
 CREATE TABLE [Фильм]
 ( 
-	[Код_фильма] int  NOT NULL ,
-	[Название]   varchar(20)  NOT NULL ,
+	[Код_фильма] int  IDENTITY(1,1) ,
+	[Название]   varchar(100)  NOT NULL ,
 	[Длительность] int  NULL ,
-	[Жанр]           varchar(20)  NULL ,
+	[Жанр]           varchar(50)  NULL ,
 	[Год_выхода] int  NULL,
 	CONSTRAINT [XPKФильм] PRIMARY KEY  NONCLUSTERED ([Код_фильма] ASC)
 )
@@ -23,28 +23,24 @@ CREATE TABLE [Фильм]
 CREATE TABLE [Киностудия]
 ( 
 	[Код_киностудии] int  IDENTITY(1,1) ,
-	[Название]   varchar(20)  NOT NULL ,
-	[Город_расположения] varchar(20)  NULL ,
+	[Название]   varchar(100)  NOT NULL ,
+	[Город_расположения] varchar(50)  NULL ,
 	[Дата_основания] datetime  NULL ,
 	CONSTRAINT [XPKКиностудия] PRIMARY KEY  NONCLUSTERED ([Код_киностудии] ASC)
 )
 
 CREATE TABLE [Актёр_Фильм]
 ( 
-	[Код_актёра] int  NOT NULL ,
-	[Код_фильма] int  NOT NULL ,
-	CONSTRAINT [XPKАктёр_Фильм] PRIMARY KEY  CLUSTERED ([Код_актёра] ASC, [Код_фильма] ASC)
+	[Код_актёра] int NULL ,
+	[Код_фильма] int  NULL ,
 )
-
 
 
 CREATE TABLE [Киностудия_Фильм]
 ( 
-	[Код_киностудии] int  NOT NULL ,
-	[Код_фильма] int  NOT NULL ,
-	CONSTRAINT [XPKКиностудия_Фильм] PRIMARY KEY  CLUSTERED ([Код_киностудии] ASC,[Код_фильма] ASC)
+	[Код_киностудии] int  NULL ,
+	[Код_фильма] int  NULL ,
 )
-
 
 CREATE TABLE [Контракты]
 ( 
